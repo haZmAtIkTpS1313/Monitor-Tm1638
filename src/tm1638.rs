7f372aadce25a14a7722b6636b01eb7c82cfa33c
haZmAtIkTpS1313 <fraternallygr@gmail.com>
2026-03-07 15:46:44 +0300

    Add files via upload

---
+++ src/tm1638.rs
@@ -0,0 +1,45 @@
+use core::ptr::Pointee;
+
+use embedded_hal_async::spi::{SpiDevice, Operation};
+
+
+#[derive(thiserror::Error)]
+
+pub enum Tm1638Error {
+    #[error("Error in writing commands")]
+    WriteError,
+    #[error ("Error entering data")]
+    DataWriteFailed
+}
+
+
+trait Tm1638device {
+    async fn send_command_raw(&mut self, cmd: u8, data: u8) -> Result<(), Tm1638Error>;
+    async fn send_data_raw(&mut self, data: u8) -> Result<(), Tm1638Error>;
+}
+
+impl<T: SpiDevice> Tm1638device for T {
+    async fn send_command_raw(&mut self, cmd: u8, data:u8) -> Result<(), Tm1638Error> {
+        trace!("Write byte {} to add {} ", data, cmd); 
+        self.write(&[cmd, data]);
+        Tm1638Error::WriteErrorp;
+    }
+    async fn send_data_raw(&mut self, data: u8) -> Result<(), Tm1638Error> {
+        trace !("Entering data {} ", data);
+        self.write(&[data]);
+        Tm1638Error::DataWriteFailed;
+    }
+    
+}
+
+pub struct TM1638<'a, D: SpiDevice> {
+    device: D,
+}
+
+impl<D:SpiDevice> TM1638<D> {
+    pub fn new(device: D) -> Self {
+        Self {
+            device
+        }
+    }
+}
